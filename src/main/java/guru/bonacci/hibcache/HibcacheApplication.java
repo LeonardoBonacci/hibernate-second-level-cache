a6a6ce58b5543576e9610d6a8ea9e44b25946c36
jeff_innovalor <jeffrey.vanhelden@innovalor.nl>
2019-04-25 18:20:33 +0200

    first commit

---
+++ src/main/java/guru/bonacci/hibcache/HibcacheApplication.java
@@ -0,0 +1,13 @@
+package guru.bonacci.hibcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibcacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibcacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/bonacci/hibcache/HibcacheApplicationTests.java
@@ -0,0 +1,16 @@
+package guru.bonacci.hibcache;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HibcacheApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
